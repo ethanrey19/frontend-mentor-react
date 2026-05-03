ee8180f43d75e1efd7d35643b75f2ec2066f4fbd
ebud6 <ebud6805@gmail.com>
2026-05-03 18:30:58 -0500

    Blog Page Finished

--- src/projects/BlogCard/componets/BlogCard.jsx
+++ src/projects/BlogCard/componets/BlogCard.jsx
@@ -1,11 +1,22 @@
 import "./BlogCard.css";
+import blogImage from "../assets/images//illustration-article.svg"
+import avatar from "../assets/images/image-avatar.webp"
 
 function BlogCard() {
     return (
         <div className="blog-card">
-            <h2>Blog Card Title</h2>
-            <p>This is a brief description of the blog post. It gives an overview of the content.</p>
-            <button>Read More</button>
+            <img src={blogImage} alt="Blog thumbnail" className="blog-thumbnail"/>
+            <p className="blog-category">Learning</p>
+            <p className="blog-date">Published 21 Dec 2023</p>
+            <header>
+                <h2 className="blog-title">HTML & CSS foundations</h2>
+            </header>
+            <p className="blog-description">These languages are the backbone of every website, defining the structure, content and prensentation</p>
+            <div className="author">
+                <img src={avatar} alt="avatar" className="author-avatar"></img>
+                <p className="author-name">Greg Hooper</p>
+            </div>
+            
         </div>
     );
 }
