3975d69db5713d10c8234a4a4e7b27530ff78811
ebud6 <ebud6805@gmail.com>
2026-05-05 11:34:15 -0500

    reformat blogpage

--- src/projects/BlogCard/pages/BlogPage.jsx
+++ src/projects/BlogCard/pages/BlogPage.jsx
@@ -5,15 +5,15 @@ import usePageBackground from "../../../hooks/usePageBackground";
 import usePageTitle from "../../../hooks/usePageTitle";
 
 function BlogPage() {
-    usePageBackground("hsl(47, 88%, 63%)");
-    usePageTitle("Blog Card - Ethan Reynolds");
-    
-    return (
-        <div className="blog-page">
-            <BlogCard />
-            <HomeButton />
-        </div>
-    );
+  usePageBackground("hsl(47, 88%, 63%)");
+  usePageTitle("Blog Card - Ethan Reynolds");
+
+  return (
+    <div className="blog-page">
+      <BlogCard />
+      <HomeButton />
+    </div>
+  );
 }
 
 export default BlogPage;
